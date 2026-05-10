b58493c23946af81c77fc3d86d7249bc3790942c
Eric Garcia <eric.d.r.garcia@gmail.com>
2026-05-10 19:52:45 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Define the the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from Express!');
+// });
+
+/**
+  * Routes
+  */
+app.get('/', (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });});
+
+app.get('/organizations', (req, res) => {
+    const title = 'Organizations';
+    res.render('organizations', { title });});
+
+app.get('/projects', (req, res) => {
+    const title = 'Projects';
+    res.render('projects', { title });});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
